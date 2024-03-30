2a9b761215b555997c809ddfc211b7baa79c0741
MWise707 <markezrawiseman@gmail.com>
2024-03-29 18:18:10 -0700

    basic header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import './header.css'
+
+const handleClick = () => {
+  // logic that will happen
+}
+const Header = () => {
+  return (
+    <>
+      <div>
+        <div className='header-container'>
+          <p>Lights Out</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
