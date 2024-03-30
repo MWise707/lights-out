32fe208b9112ab868fc21e3bf933360f629ac8d5
MWise707 <markezrawiseman@gmail.com>
2024-03-29 17:40:17 -0700

    tile box fix

--- src/components/tile/Tile.jsx
+++ src/components/tile/Tile.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import './tile.css'
+import { useState } from "react";
+import "./tile.css";
 
 const Tile = () => {
-
   return (
     <>
-      <div className='tile-container'>
-        <p>Hello World</p>
+      <div className="tile-container">
+        <div className="lightbulb">
+          <p>Hello World</p>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Tile
+export default Tile;
