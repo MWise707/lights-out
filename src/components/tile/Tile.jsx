fde3c993754f47f92bd2fda5bb566da79135ce6d
MWise707 <markezrawiseman@gmail.com>
2024-03-29 17:24:24 -0700

    create tile

---
+++ src/components/tile/Tile.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './tile.css'
+
+const Tile = () => {
+
+  return (
+    <>
+      <div className='tile-container'>
+        <p>Hello World</p>
+      </div>
+    </>
+  )
+}
+
+export default Tile
