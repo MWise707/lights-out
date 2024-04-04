d599aba4159f10f754b1f432ff1c03665e40f03c
MWise707 <markezrawiseman@gmail.com>
2024-04-03 18:40:12 -0700

    config change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lights-out/',
+  // base: '/lights-out/',
   plugins: [react()],
 });
